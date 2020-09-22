31c6b87d9edd1ff4c0060ed2ca1ac5d88a9d97e6
AbhinavGor <abhinav20016@gmail.com>
2020-09-22 15:17:06 +0530

    Added alternative TRIE implementatino

--- speller.cpp
+++ speller.cpp
@@ -108,8 +108,36 @@ int printAutoSuggestions(TRIENode* root, const string query){
     return 0;
 }
 
+string hexToASCII(string hex) 
+{ 
+    // initialize the ASCII code string as empty. 
+    string ascii = ""; 
+    for (size_t i = 0; i < hex.length(); i += 2) 
+    { 
+        // extract two characters from hex string 
+        string part = hex.substr(i, 2); 
+  
+        // change it into base 16 and  
+        // typecast as the character 
+        char ch = stoul(part, nullptr, 16); 
+  
+        // add this char to final ASCII string 
+        ascii += ch; 
+    } 
+    return ascii; 
+} 
+
+void display(struct TRIENode* root, char str[], int level) 
+{ 
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+    {
+        cout<<hexToASCII(root->children[i])<<"\n";
+    }
+    
+} 
+
 int main(){
-    string x;
+    char x[10000];
     struct TRIENode* root = getNode();
     fstream file;
     string word, t, q, filename;
@@ -123,10 +151,15 @@ int main(){
     cout<<"Enter the word:\n"<<endl;
 
     cin>>x;
+    display(root, x, 10);
 
-    int comp = printAutoSuggestions(root, x);
+    // int comp = printAutoSuggestions(root, x);
 
-    if(comp == -1){
-        cout<<"No similar words found.\n"<<endl;
-    }
+    // if(comp == -1){
+    //     cout<<"No similar words found.\n"<<endl;
+    // }else if(comp == 0){
+    //     cout<<"The word you entered is spelled incorrectly.\n"<<endl;
+    // }
+
+    return 0;
 }
