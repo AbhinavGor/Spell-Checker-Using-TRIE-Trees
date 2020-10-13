db3c8d038126454122c4641a35e8e7d8594887b3
AbhinavGor <abhinav20016@gmail.com>
2020-10-13 17:37:41 +0530

    Finished code for BKTree functions

--- BKTree.cpp
+++ BKTree.cpp
@@ -103,3 +103,79 @@ void BKTree::recursiveSearch(Node* curNode, vector<string>& suggestions, string 
         child = child->rightSibling;
     }
 }
+
+bool BKTree::inRange(size_t curDist, size_t minDist, size_t maxDist){
+    return (minDist <= curDist && maxDist >= curDist);
+}
+
+void BKTree::printSuggestions(vector<string>& suggestions, bool wordFound){
+    if(wordFound){
+        cout<<"Word spelled correctly."<<endl;
+    }else if(suggestions.empty()){
+        cout<<"No suggestions found."<<endl;
+    }else{
+        cout<<"Did you mean: ";
+        for(int i = 0; i < suggestions.size() - 1; i++){
+            cout<<suggestions[i]<< ", ";
+        }
+
+        cout<<suggestions[suggestions.size() - 1]<<"?"<<endl;
+    }
+}
+
+//https://en.wikipedia.org/wiki/Levenshtein_distance
+size_t BKTree::levenshteinDistance(string w1, string w2){
+    if(w1.length() == 0){
+        return w2.length();
+    }
+    if(w2.length() == 0){
+        return w1.length();
+    }
+
+    size_t n_w1 = w1.length();
+    size_t n_w2 = w2.length();
+
+    int cost;
+    
+    int d[n_w1 + 1][n_w2 + 1];
+
+    for(int i = 0; i < n_w1; i++){
+        d[i][0] = i;
+    }
+
+    for(int i = 0; i < n_w2; i++){
+        d[0][i] = i;
+    }
+
+    for (int i = 1; i <= n_w1; i++)
+    {
+        for (int j = 1; j <= n_w2; j++)
+        {
+            
+            cost = (w1[i - 1] == w2[j - 1]) ? 0 : 1;
+            
+            d[i][j] = min(d[i - 1][j] + 1,
+                          d[i][j - 1] + 1,
+                          d[i - 1][j - 1] + cost);
+        }
+    }
+    
+    return d[n_w1][n_w2];
+
+}
+
+int BKTree::min(int a, int b, int c){
+    int min = a;
+    if(b < min)
+        min = b;
+    if(c < min)
+        min = c;
+    
+    return min;
+}
+
+void BKTree::cleanString(basic_string<char>& s){
+    for(basic_string<char>::iterator p = s.begin(); p != s.end(); ++p){
+        *p = tolower(*p);
+    }
+}
